0625cc33d268a765c245ee19ca18917d599ae91e
Alexandra Samoilova <avsamoilava@gmail.com>
2022-08-06 20:42:06 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.l = 0;
+        this.r = 0;
+        this.m = 0;
+    }
 
     setRange(min, max) {
-
+        this.r = max;
+        this.l = min;
     }
 
     guess() {
-
+        this.m = Math.ceil((this.l + this.r) / 2); /* //* так же работает и через Math.round() */
+        return this.m;
     }
 
     lower() {
-
+        this.setRange(this.l, this.m)
     }
 
     greater() {
-
+        this.setRange(this.m, this.r)
     }
 }
 
